508b031c3593151150f492fc812c16c6b477f113
Rajesh Nathani <rajeshnathani055@gmail.com>
2020-12-14 19:54:49 +0530

    Update app_wsgi.py

--- app_wsgi.py
+++ app_wsgi.py
@@ -23,7 +23,8 @@ def home():
     else:
         css = "<link rel='stylesheet' href ='https://bootswatch.com/4/flatly/bootstrap.min.css'>"
     template = '''
-            <html>
+         <!DOCTYPE html>
+         <html>
                 <head>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
