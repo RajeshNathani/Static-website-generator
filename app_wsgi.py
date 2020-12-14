e92b4f5a4fc7906885469b77cd329e670a725044
Rajesh Nathani <rajeshnathani055@gmail.com>
2020-12-14 19:49:05 +0530

    Update app_wsgi.py

--- app_wsgi.py
+++ app_wsgi.py
@@ -25,6 +25,8 @@ def home():
     template = '''
             <html>
                 <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
                     <title>'''+title+'''</title>
                         '''+css+'''
                 </head>
